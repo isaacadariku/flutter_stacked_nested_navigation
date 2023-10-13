356896ac4d6a73785fb2dd99f9f16040499dcd69
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2023-10-13 20:26:57 +0100

    update UI 👨🏾‍💻

--- lib/ui/views/chat_details/chat_details_view.mobile.dart
+++ lib/ui/views/chat_details/chat_details_view.mobile.dart
@@ -8,15 +8,52 @@ class ChatDetailsViewMobile extends ViewModelWidget<ChatDetailsViewModel> {
 
   @override
   Widget build(BuildContext context, ChatDetailsViewModel viewModel) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello, MOBILE UI!',
-          style: TextStyle(
-            fontSize: 35,
-            fontWeight: FontWeight.w900,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(viewModel.chat.title),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: viewModel.chat.messages.length,
+              itemBuilder: (context, index) {
+                final message = viewModel.chat.messages[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 15,
+                    child: Text(message.sender[0] + message.sender[1]),
+                  ),
+                  title: Text(message.sender),
+                  subtitle: Text(message.message),
+                  trailing: Text(
+                    "${message.time.day}/${message.time.month}/${message.time.year}",
+                  ),
+                );
+              },
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Type a message",
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
