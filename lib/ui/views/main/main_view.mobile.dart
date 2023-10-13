c29c88878ec4d861a38a24a8b75936a494218feb
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2023-10-13 19:57:56 +0100

    Update main_view.mobile.dart

--- lib/ui/views/main/main_view.mobile.dart
+++ lib/ui/views/main/main_view.mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stacked_nested_navigation/app/app.router.dart';
 import 'package:stacked/stacked.dart';
 
 import 'main_viewmodel.dart';
@@ -8,15 +9,32 @@ class MainViewMobile extends ViewModelWidget<MainViewModel> {
 
   @override
   Widget build(BuildContext context, MainViewModel viewModel) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello, MOBILE UI!',
-          style: TextStyle(
-            fontSize: 35,
-            fontWeight: FontWeight.w900,
+    return StackedTabsScaffold(
+      routes: const [
+        MailNavigationRoute(),
+        ChatNavigationRoute(),
+      ],
+      bottomNavigationBuilder: (_, tabsRouter) => NavigationBar(
+        selectedIndex: tabsRouter.activeIndex,
+        onDestinationSelected: tabsRouter.setActiveIndex,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(
+              tabsRouter.activeIndex == 0 //
+                  ? Icons.mail //
+                  : Icons.mail_outlined,
+            ),
+            label: 'Mail',
           ),
-        ),
+          NavigationDestination(
+            icon: Icon(
+              tabsRouter.activeIndex == 1
+                  ? Icons.chat_bubble_rounded
+                  : Icons.chat_bubble_outline_rounded,
+            ),
+            label: 'Chat',
+          ),
+        ],
       ),
     );
   }
